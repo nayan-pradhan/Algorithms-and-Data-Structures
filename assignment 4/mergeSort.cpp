339bd0c76837b00de6a39c864804e33017a6b999
Nayan <nayan.pradhan@hotmail.com>
2020-02-27 10:32:28 +0100

    working mergeSort

--- assignment 4/mergeSort.cpp
+++ assignment 4/mergeSort.cpp
@@ -1,81 +1,78 @@
 #include <iostream>
 
-void mergeSort (int arr[], int left, int right);
-void merge (int arr[], int left, int right, int mid);
-void print (int arr[], int size);
+void print (int *arr, int size);
+void mergeSort (int *arr, int left, int right);
+void merge (int *arr, int left, int right, int mid);
 
 int main () {
     int size;
-    std::cout << "Enter size of array: ";
+    std::cout << "Enter size: ";
     std::cin >> size;
     int arr[size];
     for (int i = 0; i < size; i++) {
         std::cout << "Elem " << i+1 << ": ";
         std::cin >> arr[i];
     }
-    std::cout << "Unsorted Array: ";
-    print(arr, size);
-    mergeSort(arr, 0, size-1);
-    std::cout << "Sorted Array: ";
-    print(arr, size);
+    std::cout << "Array without sorting: " << std::endl;
+    print (arr, size);
+
+    mergeSort (arr, 0, size-1);
+
+    std::cout << "Array after sorting: " << std::endl;
+    print (arr, size);
     return 0;
 }
 
-void mergeSort (int arr[], int left, int right) {
+void print (int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void mergeSort (int *arr, int left, int right) {
+    int mid;
     if (left < right) {
-        int mid = (left+right)/2;
+        mid = left + ((right - left) / 2) ;
         mergeSort (arr, left, mid);
         mergeSort (arr, mid+1, right);
         merge (arr, left, right, mid);
     }
 }
 
-void merge (int arr[], int left, int right, int mid) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int L[n1];
-    int R[n2];
-    for (int i = 0; i < n1; i++) {
+void merge (int *arr, int left, int right, int mid) {
+    int Lsize = mid - left + 1;
+    int Rsize = right - mid;
+    int L[Lsize];
+    int R [Rsize];
+    for (int i = 0; i < Lsize; i++) {
         L[i] = arr[left + i];
     }
-    for (int j = 0; j < n2; j++) {
+    for (int j = 0; j < Rsize; j++) {
         R[j] = arr[mid + 1 + j];
     }
     int a = 0;
     int b = 0;
     int k = left;
-    while (a < n1 && b < n2) {
-        if (L[a] <= R[b]) {
-            std::cout << L[a] << "<" << R[b]<< std::endl;
+    while (a < Lsize && b < Rsize) {
+        if (L[a] < R[b]) {
             arr[k] = L[a];
             a++;
         }
-        else if (L[a] > R[b]) {
-            std::cout << L[a] << ">" << R[b]<< std::endl;
+        else {
             arr[k] = R[b];
             b++;
         }
         k++;
     }
-    std::cout << " -------- " << std::endl;
-    while (a < n1) {
-                std::cout << "a<n1" << std::endl;
-        arr[k] = R[a];
-        k++;
+    while (a < Lsize) {
+        arr[k] = L[a];
         a++;
-    }
-    while (b < n2) {
-                    std::cout <<"b<n2" <<std::endl;
-        arr[k] = L[b];
         k++;
+    }
+    while (b < Rsize) {
+        arr[k] = R[b];
         b++;
+        k++;
     }
-
-}
-
-void print (int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
 }
