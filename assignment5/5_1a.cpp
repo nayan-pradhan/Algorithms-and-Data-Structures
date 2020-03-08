97c26489cc833cdb7e18bf4ca3fa020e6d040ee1
Nayan <nayan.pradhan@hotmail.com>
2020-03-08 18:24:38 +0100

    comments added

--- assignment5/5_1a.cpp
+++ assignment5/5_1a.cpp
@@ -68,25 +68,26 @@ int closedForm (int n) {
 }
 
 void matrixRep (int n) {
-    if (n == 0) {
+    if (n == 0) { //if n is 0, the fibonacci is 0
         std::cout << 0;
     }
     else {
-        int myMatrix[2][2] = {{1,1}, {1,0}};
-        if (n % 2 == 0) {
-            matrixPower (myMatrix, n/2);
-            matrixMultiply (myMatrix, myMatrix);
+        int myMatrix[2][2] = {{1,1}, {1,0}}; //making my matrix
+        if (n % 2 == 0) { //if even number n
+            matrixPower (myMatrix, n/2); //power with n/2 
+            matrixMultiply (myMatrix, myMatrix); //multiply the two parts
         }
-        else {
-            int refMatrix[2][2] = {{1,1}, {1,0}};
-            matrixPower (myMatrix, (n-1)/2);
-            matrixMultiply (myMatrix, myMatrix);
-            matrixMultiply (myMatrix, refMatrix);
+        else { //if odd number n
+            //a reference matrix because our matrix will get modified
+            int refMatrix[2][2] = {{1,1}, {1,0}}; 
+            matrixPower (myMatrix, (n-1)/2); //power with (n-1)/2
+            matrixMultiply (myMatrix, myMatrix); //multiply with itself
+            matrixMultiply (myMatrix, refMatrix); //multiply with ref matrix
         }
-        if (myMatrix[0][1] == myMatrix[1][0]) {
+        if (myMatrix[0][1] == myMatrix[1][0]) { //just checking
             std::cout << myMatrix[0][1] << std::endl;
         }
-        else { 
+        else { //if the check is incorrect
             std::cout << "Error!" << std::endl;
         }
     }
@@ -94,14 +95,16 @@ void matrixRep (int n) {
 
 void matrixPower (int matrix[2][2], int n) {
     if (n > 1) {
-        int refMatrix[2][2] = {{1,1}, {1,0}};
+        int refMatrix[2][2] = {{1,1}, {1,0}}; //another reference matrix
         for (int k = 2; k <= n; k++) {
+            //poiwer of n is multiplication of matrix with itself n times
             matrixMultiply (matrix, refMatrix);
         }
     }
 }
 
 void matrixMultiply (int matrixA[2][2], int matrixB[2][2]) {
+    //just a function that multiplies two matrices
     int a = ((matrixA[0][0]*matrixB[0][0]) + 
         (matrixA[0][1]*matrixB[1][0]));
     int b = ((matrixA[0][0]*matrixB[0][1]) + 
@@ -111,6 +114,7 @@ void matrixMultiply (int matrixA[2][2], int matrixB[2][2]) {
     int d = ((matrixA[1][0]*matrixB[0][1]) + 
         (matrixA[1][1]*matrixB[1][1]));
     //assigning a b c d to respective parts of matrix
+    //if this is not done, matrix values changes during operation giving error
     matrixA[0][0] = a;
     matrixA[0][1] = b;
     matrixA[1][0] = c;
