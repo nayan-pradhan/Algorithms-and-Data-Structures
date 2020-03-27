98ba5f1a05b14d3df217b8115253c61ab6153ff3
Nayan <nayan.pradhan@hotmail.com>
2020-03-27 07:59:29 +0100

    assignment

---
+++ assignment7/radixSortOld.cpp
@@ -0,0 +1,85 @@
+/* 
+    CH-231-A
+    a7.2a.ii cpp 
+    Nayan Man Singh Pradhan 
+    n.pradhan@jacobs-university.de
+*/
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+void print(std::vector<int>arr);
+void radixSort(std::vector<int>*arr);
+int largestElem(std::vector<int>arr);
+int digitAtSigFig(int elem, int s);
+int radixSortHelper(std::vector<int>*arr, int n);
+void countSort(std::vector<int>*arr);
+
+int main() {
+    std::vector<int>arr = {578, 561, 212, 1234, 1432, 4563, 121};
+    std::cout << "array before sorting: ";
+    print(arr);
+    radixSort(&arr);
+    std::cout << "array after sorting: ";
+    print(arr);
+}
+
+void print(std::vector<int>arr) {
+    std::vector<int>::iterator itr;
+    for (itr = arr.begin(); itr != arr.end(); itr++) {
+        std::cout << *itr << " ";
+    }
+    std::cout << std::endl;
+}
+
+int largestElem(std::vector<int>arr) {
+    int max = arr[0];
+    std::vector<int>::iterator itr;
+    for (itr = arr.begin(); itr != arr.end(); itr++) {
+        if (*itr > max) {
+            max = *itr;
+        }
+    }
+    return max;
+}
+
+int digitAtSigFig(int elem, int s) {
+    int power = pow (10, s-1);
+    return ((elem/power)%10);
+}
+
+void radixSort(std::vector<int>*arr) {
+    int sigFig = log10(largestElem((*arr)))+1; //number of sigFig in max elem
+    // std::vector<int>auxVector[10];
+    radixSortHelper (&(*arr), sigFig);
+}
+
+int radixSortHelper(std::vector<int>*arr, int sigFig) {
+    std::vector<int>auxVec[10];
+
+    if (sigFig == 0) {
+        return 0;
+    }
+
+    // std::vector<int>auxVec[10];
+
+    std::vector<int>::iterator itr1;
+    for (itr1 = (*arr).begin(); itr1 != (*arr).end(); itr1++) {
+        int digit = digitAtSigFig(*itr1, sigFig);
+        for (int i = 0; i < 10; i++) {
+            if (digit == i) {
+                auxVec[i].push_back(*itr1);
+                std::cout << i << ": "; //for testing
+                print (auxVec[i]); //for testing
+            }
+        }
+    }
+    std::cout << "___________________________________________" << std::endl; //for testing
+    std::vector<int>::iterator itr2;
+    for (int j = 0; j < 10; j++) {
+        for (itr2 = auxVec[j].begin(); itr2 != auxVec[j].end(); itr2++) {
+            radixSortHelper(&auxVec[j], sigFig-1);
+        }
+    }
+}
---
+++ assignment8/random.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> myVec;
+    for (int i = 0; i < 10; i++) {
+        myVec.push_back(i);
+    }
+    for (int i = 0; i < 10; i++) {
+        std::cout << myVec[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Popping ";
+    myVec.pop_back();
+    std::cout << "Popping ";
+    myVec.pop_back();
+    std::cout << "Popping ";
+    myVec.pop_back();
+
+    std::cout << std::endl;
+
+    std::vector<int>::iterator itr;
+    for (itr = myVec.begin(); itr != myVec.end(); itr++) {
+        std::cout << *itr << " ";
+    }
+}
