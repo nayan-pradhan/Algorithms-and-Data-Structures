dd006106597a84da56f2cd71e6c042f16e7a73d3
Nayan <nayan.pradhan@hotmail.com>
2020-03-30 00:00:15 +0200

    assignment 8

--- assignment8/random.cpp
+++ assignment8/random.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <vector>
 
+// int main() {
+//     std::vector<int> myVec;
+//     for (int i = 0; i < 10; i++) {
+//         myVec.push_back(i);
+//     }
+//     for (int i = 0; i < 10; i++) {
+//         std::cout << myVec[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     std::cout << "Popping ";
+//     myVec.pop_back();
+//     std::cout << "Popping ";
+//     myVec.pop_back();
+//     std::cout << "Popping ";
+//     myVec.pop_back();
+
+//     std::cout << std::endl;
+
+//     std::vector<int>::iterator itr;
+//     for (itr = myVec.begin(); itr != myVec.end(); itr++) {
+//         std::cout << *itr << " ";
+//     }
+// }
+
 int main() {
-    std::vector<int> myVec;
-    for (int i = 0; i < 10; i++) {
-        myVec.push_back(i);
+    int x = 1;
+    try {
+        if (x == 1) {
+            throw "he";
+        }
     }
-    for (int i = 0; i < 10; i++) {
-        std::cout << myVec[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "Popping ";
-    myVec.pop_back();
-    std::cout << "Popping ";
-    myVec.pop_back();
-    std::cout << "Popping ";
-    myVec.pop_back();
-
-    std::cout << std::endl;
-
-    std::vector<int>::iterator itr;
-    for (itr = myVec.begin(); itr != myVec.end(); itr++) {
-        std::cout << *itr << " ";
+    catch (...) {
+        std::cout << "caught" << std::endl;
+        exit (1);
     }
 }
