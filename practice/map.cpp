b0bd7e48681334116a24877a93134651a456e743
Nayan <nayan.pradhan@hotmail.com>
2020-05-10 23:41:18 +0200

    practice

--- practice/map.cpp
+++ practice/map.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <set>
+#include <algorithm>
 
 void print(std::map<std::string, std::string> myMap) {
     std::map<std::string, std::string>::iterator itr;
@@ -8,12 +10,13 @@ void print(std::map<std::string, std::string> myMap) {
     }
 }
 
-bool practice(std::map<std::string, std::string> Translator) {
+template <class T, class K>
+bool practice(std::map<T, K> obj) {
     std::cout << "Welcome to my german quiz!" << std::endl;
     std::cout << "Write down the english translation for the words. Good luck!" << std::endl;
     std::cout << "Enter 'QUIT' to exit game" << std::endl;
-    std::map<std::string, std::string>::iterator itr = Translator.begin();
-    std::string answer;
+    typename std::map<T,K>::iterator itr = obj.begin();
+    K answer;
     while (1) {
         std::cout << itr -> first << " -> ";
         std::cin >> answer;
@@ -22,13 +25,16 @@ bool practice(std::map<std::string, std::string> Translator) {
         }
         else if (answer == "QUIT") {
             std::cout << "Thank you for playing!" << std::endl;
-            return false;
+            exit(-1);
+        }
+        else if (answer == "admin") {
+            return true;
         }
         else {
             std::cout << "Try again!" << std::endl;
             continue;
         }
-        if (itr == Translator.end()) {
+        if (itr == obj.end()) {
             std::cout << "Congradulations, you won!" << std::endl;
             return true;
         }
@@ -46,5 +52,27 @@ int main() {
     Translator.insert(std::pair<std::string, std::string>("Termin", "Meeting")); // learn this
     //
     Translator.insert(std::pair<std::string, std::string>("Pause", "Break"));
-    practice(Translator);
+    
+    std::map<int, std::string> Numbers;
+    Numbers = {{1, "Ein"}, {2, "Zwei"}, {3, "Drei"}, {4, "Vier"}, {5, "Fünf"},
+        {6, "Sechs"}, {7, "Sieben"}, {8, "Acht"}, {9, "Neun"}, {10, "Zehn"}};
+    
+    std::cout << "Select which one you would want to practice:" << std::endl;
+    std::cout << "1. Translator" << std::endl;
+    std::cout << "2. Numbers" << std::endl;
+    std::cout << "3. All" << std::endl;
+    std::cout << "Enter 1, 2, or 3 -> ";
+    int input;
+    std::cin >> input;
+    switch(input) {
+        case 1:
+            practice(Translator);
+            break;
+        case 2:
+            practice(Numbers);
+            break;
+        default:
+            practice(Translator);
+            practice(Numbers);
+    }
 }
