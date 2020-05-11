1c471d69978d94c603f35eae0e62030b96465dbd
Nayan <nayan.pradhan@hotmail.com>
2020-05-11 09:37:29 +0200

    practice

---
+++ practice/tuple.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <tuple>
+
+int main() {
+    std::tuple<int, double, char, std::string> myTuple {1, 1.2, 'a', "hello"};
+    std::cout << std::get<0>(myTuple) << std::endl;
+    std::cout << std::get<1>(myTuple) << std::endl;
+    std::get<0>(myTuple) = std::get<0>(myTuple) + 100;
+    std::cout << std::get<0>(myTuple) << std::endl;
+}
