43d4b55621581381e479f3c9ddffe1f74351df34
Nayan <nayan.pradhan@hotmail.com>
2020-05-14 12:06:01 +0200

    practice

--- practice/TemplatesII.cpp
+++ practice/TemplatesII.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 template <class T>
-T& multiplies (T sum, T x, T n) {
+void multiplies (T &sum, T x, T n) {
     for (int i = 0; i <= n; i++) {
         sum = sum + (x*i);
     }
-    return sum;
 }
 
 int main() {
     int sum = 1;
     int x = 2;
     int n = 10;
-    std::cout << multiplies(sum, x, n) << std::endl;
+    multiplies(sum, x, n);
+    std::cout << sum << std::endl;
 }
